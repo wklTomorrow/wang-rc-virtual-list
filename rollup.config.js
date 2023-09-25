9371a2bc1f78ebdae773676741b5e4d2eab55905
wangkunlun <wangkunlun@yuanfudao.com>
2023-09-25 16:53:29 +0800

    FEAT: build for mjs

--- rollup.config.js
+++ rollup.config.js
@@ -5,12 +5,18 @@ import external from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
+import pkg from "./package.json";
 
 export default {
   input: "src/index.ts",
   output: [
     {
-      file: "dist/index.js",
+      file: pkg.main,
+      format: "cjs", // CommonJS (适用于 require 等)
+      exports: "named",
+    },
+    {
+      file: pkg.module,
       format: "esm",
     },
   ],
