15e1a0c9a7a311a367963fe40770c94605bcdc52
wangkunlun <wangkunlun@yuanfudao.com>
2024-03-18 15:07:47 +0800

    FEAT: compat obverser

--- src/virtual-list/hooks/useResizeObserver.ts
+++ src/virtual-list/hooks/useResizeObserver.ts
@@ -1,23 +1,44 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from "react";
 
 export const useResizeObserver = (dom: HTMLElement) => {
-  const [size, setSize] = useState<number>(0)
-  const resizeObserver = new ResizeObserver(entries => {
-    for (const entry of entries) {
-      if (entry.contentRect) {
-        setSize(old => old + 1)
+  const [size, setSize] = useState<number>(0);
+  const heightRef = useRef<number>(0);
+
+  let resizeObserver: any = null;
+  const config = {
+    attributes: true,
+    childList: true,
+    subtree: true,
+  };
+  if (window.ResizeObserver) {
+    resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        if (entry.contentRect) {
+          setSize((old) => old + 1);
+        }
       }
-    }
-  })
+    });
+  } else {
+    const callback = function () {
+      if (dom.offsetHeight !== heightRef.current) {
+        heightRef.current = dom.offsetHeight;
+        setSize((old) => old + 1);
+      }
+    };
+    resizeObserver = new MutationObserver(callback);
+  }
+
   useEffect(() => {
     if (dom) {
-      resizeObserver.observe(dom)
+      heightRef.current = dom.offsetHeight;
+      resizeObserver.observe(dom, config);
     }
     return () => {
       if (dom) {
-        resizeObserver.unobserve(dom)
+        resizeObserver?.unobserve(dom);
+        resizeObserver?.disconnect();
       }
-    }
-  }, [dom])
-  return size
-}
+    };
+  }, [dom]);
+  return size;
+};
